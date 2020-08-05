17301c6998e7927518352778a8baa37ef8081d4c
guiguat <guaturadzn@gmail.com>
2020-08-05 13:01:31 -0300

    Create Lecture model

--- csharp-mysql/Models/Lecture.cs
+++ csharp-mysql/Models/Lecture.cs
@@ -6,5 +6,18 @@ namespace csharp_mysql.Models
 {
     class Lecture
     {
+        public Lecture(int id = 0, string title, string speaker, string day, string hour)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.SpeakerName = speaker;
+            this.Day = day;
+            this.Hour = hour;
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SpeakerName { get; set; }
+        public string Day { get; set; }
+        public string Hour { get; set; }
     }
 }
