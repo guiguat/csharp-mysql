6ca58a39d0e4923012b01101f79630dda7c27298
guiguat <guaturadzn@gmail.com>
2020-08-05 16:27:43 -0300

    create Register Lecture UI and logic

---
+++ csharp-mysql/RegisterLecture.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace csharp_mysql
+{
+    partial class RegisterLecture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txbTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txbDate = new System.Windows.Forms.MaskedTextBox();
+            this.cbHour = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbSpeaker = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txbTitle
+            // 
+            this.txbTitle.Location = new System.Drawing.Point(76, 30);
+            this.txbTitle.Name = "txbTitle";
+            this.txbTitle.Size = new System.Drawing.Size(259, 23);
+            this.txbTitle.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Title:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.txbDate);
+            this.groupBox1.Controls.Add(this.cbHour);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbSpeaker);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txbTitle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(268, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 225);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lecture Data";
+            // 
+            // txbDate
+            // 
+            this.txbDate.Location = new System.Drawing.Point(77, 95);
+            this.txbDate.Mask = "00/00/0000";
+            this.txbDate.Name = "txbDate";
+            this.txbDate.Size = new System.Drawing.Size(106, 23);
+            this.txbDate.TabIndex = 5;
+            // 
+            // cbHour
+            // 
+            this.cbHour.FormattingEnabled = true;
+            this.cbHour.Items.AddRange(new object[] {
+            "7:30 - 8:30",
+            "8:30 - 9:30",
+            "9:30 - 10:30",
+            "10:30 - 11:30",
+            "11:30 - 12:00"});
+            this.cbHour.Location = new System.Drawing.Point(76, 127);
+            this.cbHour.Name = "cbHour";
+            this.cbHour.Size = new System.Drawing.Size(107, 23);
+            this.cbHour.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Hour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Date:";
+            // 
+            // txbSpeaker
+            // 
+            this.txbSpeaker.Location = new System.Drawing.Point(76, 63);
+            this.txbSpeaker.Name = "txbSpeaker";
+            this.txbSpeaker.Size = new System.Drawing.Size(259, 23);
+            this.txbSpeaker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Speaker:";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.monthCalendar1.Location = new System.Drawing.Point(21, 29);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 2;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // RegisterLecture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 272);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "RegisterLecture";
+            this.ShowIcon = false;
+            this.Text = "RegisterLecture";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txbTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbSpeaker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox cbHour;
+        private System.Windows.Forms.MaskedTextBox txbDate;
+    }
+}
---
+++ csharp-mysql/RegisterLecture.cs
@@ -0,0 +1,65 @@
+﻿using csharp_mysql.Controllers;
+using csharp_mysql.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace csharp_mysql
+{
+    public partial class RegisterLecture : Form
+    {
+        public RegisterLecture()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+           txbDate.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(
+                txbSpeaker.Text == String.Empty
+                || txbTitle.Text == String.Empty
+                || txbDate.Text == String.Empty
+                || cbHour.SelectedItem == null
+               ))
+            {
+                string title = txbTitle.Text;
+                string day = txbDate.Text;
+                string hour = cbHour.SelectedItem.ToString();
+                string speaker = txbSpeaker.Text;
+                try
+                {
+                    var lect = new Lecture(title, speaker, day, hour);
+                    var ctrl = new LectureController();
+                    ctrl.Insert(lect);
+                    MessageBox.Show($"Lecture \"{lect.Title}\" was succesfully registered!");
+                    ClearForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to insert in table Lecture\n Error:\n{ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in the fields properly");
+            }
+        }
+
+        private void ClearForm()
+        {
+            txbTitle.Text = "";
+            txbSpeaker.Text = "";
+            txbDate.Text = "";
+            cbHour.SelectedItem = null;
+        }
+    }
+}
