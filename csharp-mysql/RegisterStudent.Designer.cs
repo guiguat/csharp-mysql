73ea0518ddc04107fce19fc120042fc62999e7a2
guiguat <guaturadzn@gmail.com>
2020-08-05 15:03:09 -0300

    create RegisterStudent form

---
+++ csharp-mysql/RegisterStudent.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace csharp_mysql
+{
+    partial class RegisterStudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbClasses = new System.Windows.Forms.ComboBox();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.txbRA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRA = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.cbClasses);
+            this.groupBox1.Controls.Add(this.txbName);
+            this.groupBox1.Controls.Add(this.txbRA);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblRA);
+            this.groupBox1.Location = new System.Drawing.Point(29, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Data";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(338, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 156);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbClasses
+            // 
+            this.cbClasses.FormattingEnabled = true;
+            this.cbClasses.Items.AddRange(new object[] {
+            "1IND",
+            "2IND",
+            "3IND",
+            "1INN",
+            "2INN",
+            "3INN",
+            "1CTI",
+            "2CTI"});
+            this.cbClasses.Location = new System.Drawing.Point(69, 103);
+            this.cbClasses.Name = "cbClasses";
+            this.cbClasses.Size = new System.Drawing.Size(121, 23);
+            this.cbClasses.TabIndex = 2;
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(70, 65);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(287, 23);
+            this.txbName.TabIndex = 1;
+            // 
+            // txbRA
+            // 
+            this.txbRA.Location = new System.Drawing.Point(69, 30);
+            this.txbRA.Name = "txbRA";
+            this.txbRA.Size = new System.Drawing.Size(143, 23);
+            this.txbRA.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Class:";
+            // 
+            // lblRA
+            // 
+            this.lblRA.AutoSize = true;
+            this.lblRA.Location = new System.Drawing.Point(19, 33);
+            this.lblRA.Name = "lblRA";
+            this.lblRA.Size = new System.Drawing.Size(25, 15);
+            this.lblRA.TabIndex = 0;
+            this.lblRA.Text = "RA:";
+            // 
+            // RegisterStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 269);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "RegisterStudentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register Student";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbClasses;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox txbRA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRA;
+    }
+}
---
+++ csharp-mysql/RegisterStudent.cs
@@ -0,0 +1,54 @@
+﻿using csharp_mysql.Controllers;
+using csharp_mysql.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace csharp_mysql
+{
+    public partial class RegisterStudentForm : Form
+    {
+        public RegisterStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ra = txbRA.Text;
+            string name = txbName.Text;
+            string student_class = cbClasses.SelectedItem.ToString();
+            
+            if(!(ra == String.Empty || name == String.Empty || student_class == String.Empty))
+            {
+                try
+                {
+                    var student = new Student(int.Parse(ra), name, student_class);
+                    var ctrl = new StudentController();
+                    ctrl.Insert(student);
+                    MessageBox.Show($"Student \"{student.Name}\" was succesfully registered!");
+                    ClearForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Not able to insert into database\nError:\n{ex}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in the fields properly");
+            }
+        }
+
+        private void ClearForm()
+        {
+            txbName.Text = "";
+            txbRA.Text = "";
+            cbClasses.SelectedItem = null;
+        }
+    }
+}
